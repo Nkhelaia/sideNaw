b733718ab36dd217ecddda1652d84a793fa1b6ad
Nanuka Khelaia <98689876+Nkhelaia@users.noreply.github.com>
2022-03-06 16:03:34 +0400

    sidebar navigation
    Animated Sidebar navigation with html css and js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('openBtn').addEventListener('click', function(){
+    document.getElementById("leftMenu").style.width = "250px";
+})
+document.getElementById('closeBtn').addEventListener('click', function(){
+    document.getElementById("leftMenu").style.width = "0";
+})
